ce2ec45071c56bee356a6735b88ff85be897d707
Jakub Vaclavik <jakub.vaclavik@muziker.com>
2025-10-18 18:03:39 +0200

    Add profile page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,11 +7,11 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(
       :sign_up,
-      keys: %i[name height_cm weight_kg avatar]
+      keys: %i[given_name family_name weight height age avatar]
     )
     devise_parameter_sanitizer.permit(
       :account_update,
-      keys: %i[name height_cm weight_kg avatar]
+      keys: %i[given_name family_name weight height age avatar]
     )
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   get 'pages/index'
   get 'pages/test'
+  get 'profile', to: 'users#show'
   devise_for :users
+
+  devise_scope :user do
+    get 'profile/edit', to: 'devise/registrations#edit', as: :edit_profile
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
